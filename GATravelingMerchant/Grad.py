ae2d0b04db0877a0b697c19efcc5d778102dab5d
VajdaBela <vajdabela01@gmail.com>
2018-12-14 14:48:55 +0100

    Grad i Put napravljen
    Osnovne stavri za Grad i Put
    -Grad:
      napravio init za grad
      napravio funkciju koja ucitava gradove
      napravio str funkciju (naznam zasto)
    -Put
      napravio init
      napravio staticku metodu za izracunavanje duzine puta
    Grad je testiran, Put nije

--- GATravelingMerchant/Grad.py
+++ GATravelingMerchant/Grad.py
@@ -1,1 +1,26 @@
-print("dbz")
+class Grad:
+    'grad je obelezen sa x i y kordinatom'
+    def __init__(self, linija):
+        tokeni = linija.split(" ")
+        self.ime = tokeni[0]
+        self.x = float(tokeni[1])
+        self.y = float(tokeni[2])
+
+    def udaljenost(self, other):
+        return ((self.x - other.x)**2 + (self.y - other.y)**2) ** 0.5
+
+    def __str__(self):
+        return "ime = " + self.ime + " x = " + self.x + " y = " + self.y
+
+    @classmethod
+    def ucitaljIzFile(cls, file, storage):
+        for linija in file:
+            tacka = Grad(linija)
+            storage[tacka.ime] = tacka
+
+
+if __name__ == '__main__':
+    ListaGradova = {}
+    f = open("data_tsp.txt", 'r')
+    Grad.ucitaljIzFile(f, ListaGradova)
+    print()
--- GATravelingMerchant/Put.py
+++ GATravelingMerchant/Put.py
@@ -1,1 +1,15 @@
-print("filipini")
+sviGradovi = {}
+
+class Put:
+
+    def __init__(self, gradovi):
+        self.gradovi = gradovi
+        self.duzinaPuta = Put.izracunajPut(self.gradovi)
+
+
+    @staticmethod
+    def izracunajPut(nizGradova):
+        zbir = 0
+        for i in range(len(nizGradova) - 1):
+            zbir = nizGradova[i].udaljenost(nizGradova[i+1])
+        return zbir
